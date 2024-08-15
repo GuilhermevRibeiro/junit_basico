3ee919531e6c0a410417470e489b311e53bfcc48
Lucas Pereira Gomes Linard <lucas.linard@dinfor.uniceub>
2024-08-15 08:44:29 -0300

    inclui demais operacoes

--- src/test/java/br/com/ceub/calculadora/CalculadoraTest.java
+++ src/test/java/br/com/ceub/calculadora/CalculadoraTest.java
@@ -1,16 +1,22 @@
 package br.com.ceub.calculadora;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CalculadoraTest {
+    Calculadora calculadora;
+    // Defina um delta pequeno para a comparação de floats
+    private static final double DELTA = 1e-10;
+
+    @Before
+    public void setUp() throws Exception {
+        calculadora = new Calculadora();
+    }
 
     @Test
     public void somaDoisInteiros() {
-        // Prepare
-        Calculadora calculadora = new Calculadora();
-
         // Execute
         int resultado = calculadora.soma(1,3);
 
@@ -20,9 +26,6 @@ public class CalculadoraTest {
 
     @Test
     public void somaDoisZeros() {
-        // Prepare
-        Calculadora calculadora = new Calculadora();
-
         // Execute
         int resultado = calculadora.soma(0,0);
 
@@ -32,13 +35,38 @@ public class CalculadoraTest {
 
     @Test
     public void somaZeroEMaisUm() {
-        // Prepare
-        Calculadora calculadora = new Calculadora();
-
         // Execute
         int resultado = calculadora.soma(0,7);
 
         // Assert
         assertEquals(7, resultado);
     }
+
+    @Test
+    public void subtracao() {
+        // Execute
+        int resultado = calculadora.subtracao(5, 3);
+
+        // Assert
+        assertEquals(2, resultado);
+    }
+
+    @Test
+    public void multiplicacao() {
+        int resultado = calculadora.multiplicacao(3,4);
+
+        assertEquals(12,resultado);
+    }
+
+    @Test
+    public void divisao() {
+        double resultado = calculadora.divisao(10, 2);
+        assertEquals(5.0, resultado, DELTA);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void divisaoPorZeroDeveLancarException() {
+        calculadora.divisao(10, 0);
+    }
+
 }
